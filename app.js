0aa45e62ddb85ca31e7ddef3517daac28b62d4fb
Jackson <wthompson4@una.edu>
2024-01-25 09:53:09 -0600

    changes for enneagram 8w1

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
   res.send('Hello World From Express')
 })
 
+app.get('/enneagram', function (req, res) {
+    res.sendFile(__dirname + '/jtenn.html')
+  })
+
 //app.listen(3000)
 
 app.listen(port, () => {
